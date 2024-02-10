3359de99c5e8bdf8ad2c4ab337207e55589f86e6
Braeden Cramer <b@cramerfamily.org>
2024-02-09 23:26:39 -0700

    Added code for challenge 1c

---
+++ Challenge1c/Guessthenumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    // Generate a random number between 1 and 10
+    Random random = new Random();
+    int randomNumber = random.Next(1, 11);
+    int attempts = 0;
+
+    Console.WriteLine("Welcome to the Quirky Number Guessing Game!");
+
+    while (true)
+    {
+      Console.Write("Guess a number between 1 and 10: ");
+      int guess = int.Parse(Console.ReadLine());
+      attempts++;
+
+      // Check if the guess is equal to the random number
+      if (guess == randomNumber)
+      {
+        Console.WriteLine($"Congratulations! You guessed the number {randomNumber} in {attempts} attempts.");
+        break;
+      }
+      // Check if the guess is lower than the random number
+      else if (guess < randomNumber)
+      {
+        Console.WriteLine("Too low! Try again.");
+      }
+      // The guess must be higher than the random number
+      else
+      {
+        Console.WriteLine("Too high! Try again.");
+      }
+    }
+  }
+}
---
+++ Challenge1c/foods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    // Prompt the user to enter their three favorite foods
+    Console.WriteLine("Enter your three favorite foods:");
+
+    // Create an array to store the food items
+    string[] favoriteFoods = new string[3];
+
+    // Read the user's input and store it in the array
+    for (int i = 0; i < favoriteFoods.Length; i++)
+    {
+      Console.Write($"Food {i + 1}: ");
+      favoriteFoods[i] = Console.ReadLine();
+    }
+
+    // Display each food item with a message
+    Console.WriteLine("Here are your favorite foods:");
+    foreach (string food in favoriteFoods)
+    {
+      Console.WriteLine("I absolutely enjoy " + food + "!");
+    }
+  }
+}
