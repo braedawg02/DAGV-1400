7030ff407f7ac4adcf0f467e7b935da1f4f550d0
Braeden Cramer <b@cramerfamily.org>
2024-03-08 22:50:17 -0700

    Challenge 4b

--- Project2/Assets/DestroyOutOfBounds.cs
+++ Project2/Assets/DestroyOutOfBounds.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
+    private float topBound = 30.0f; // The upper boundary for the game object's position
+    private float lowerBound = -10.0f; // The lower boundary for the game object's position
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Code for initialization goes here
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
+        if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // Destroy the game object if it goes above the top boundary
         }
-        else if(transform.position.z < lowerBound)
-        {   
-            Debug.Log("Game Over!");
-            Destroy(gameObject);
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!"); // Log "Game Over!" if the game object goes below the lower boundary
+            Destroy(gameObject); // Destroy the game object
         }
-        
-        
     }
 }
---
+++ challenge 4b/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float shootCooldown = 1.0f;
+    private float lastShootTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog if enough time has passed since last shoot
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastShootTime + shootCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastShootTime = Time.time;
+        }
+    }
+}
